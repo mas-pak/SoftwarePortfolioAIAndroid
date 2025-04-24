937fd519f60c7cd8761309b7bedb68b5c6381079
mas-pak <mas4pak@gmail.com>
2025-04-24 17:24:15 +0500

    Add

---
+++ about.py
@@ -0,0 +1,10 @@
+full_name = "Mas-Pak"
+experience = 1
+hourly_rate = 15.75
+available = True
+print("Freelancer Profile:")
+print("Name:", full_name)
+print("Experience:", experience)
+print("Hourly Rate:", hourly_rate)
+print("Available for Work:", available)
+print("Freelancer Profile:""\nName:" + str(full_name) +"\nExperience:" + str(experience) +"\nHourly Rate:" + str(hourly_rate) +"\nAvailable for Work:" + str(available))
